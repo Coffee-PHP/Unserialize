c618afba25e2fcaf8272f5550c3a6d125e958888
Danny Damsky <dannydamsky99@gmail.com>
2020-09-13 21:08:44 +0300

    Added quality tools

--- src/Contract/ArrayUnserializerInterface.php
+++ src/Contract/ArrayUnserializerInterface.php
@@ -21,10 +21,9 @@
  * @since 2020-07-16
  */
 
-declare (strict_types=1);
+declare(strict_types=1);
 
 namespace CoffeePhp\Unserialize\Contract;
-
 
 use CoffeePhp\Unserialize\Exception\UnserializeException;
 
